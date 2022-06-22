8c1c41c7ea5c470360f8e958baef5fb0dc8d0edf
baito <2606936464@qq.com>
2022-06-22 12:28:45 +0800

    初始化

---
+++ main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+
+// 分装请求接口
+import '@/utils/http.js'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App
+})
+app.$mount()
